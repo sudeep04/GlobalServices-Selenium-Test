24aaab584d37afdf39a9463aa0266dfaac17ddd2
Sudeep Reddy <sudeep04@gmail.com>
2018-06-11 18:38:43 +0530

    Add project files.

---
+++ GlobalServices_SeleniumTest/Class1.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace SeleniumTest
+{
+    [TestFixture]
+    public class CheckDevServerHomePage
+    {
+
+        [Test]
+        public void site_header_is_on_home_page()
+        {
+            IWebDriver browser = new FirefoxDriver();
+            //Firefox's proxy driver executable is in a folder already
+            //  on the host system's PATH environment variable.
+            browser.Navigate().GoToUrl("http://dev.cmacspro.com");
+            IWebElement header = browser.FindElement(By.Id("site - header"));
+    
+            Assert.True(header.Displayed);
+            browser.Close();
+        }
+    }
+}
